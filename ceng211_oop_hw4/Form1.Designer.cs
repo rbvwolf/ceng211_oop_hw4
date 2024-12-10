fb314037c626033fab06c81676fe4650f22d813a
karay <karay@DESKTOP-FVSF267>
2024-12-10 23:52:23 +0300

    Add project files.

---
+++ ceng211_oop_hw4/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ceng211_oop_hw4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(459, 89);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(256, 154);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 9F);
+            textBox1.Location = new Point(97, 86);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(256, 157);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(97, 347);
+            button1.Name = "button1";
+            button1.Size = new Size(256, 65);
+            button1.TabIndex = 2;
+            button1.Text = "Useless Button";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.Location = new Point(459, 270);
+            button2.Name = "button2";
+            button2.Size = new Size(256, 65);
+            button2.TabIndex = 2;
+            button2.Text = "READ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(119, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(206, 37);
+            label1.TabIndex = 3;
+            label1.Text = "Write to the File";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(470, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(232, 37);
+            label2.TabIndex = 3;
+            label2.Text = "Read from the File";
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 15F);
+            button3.Location = new Point(97, 270);
+            button3.Name = "button3";
+            button3.Size = new Size(256, 65);
+            button3.TabIndex = 2;
+            button3.Text = "WRITE";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 15F);
+            button4.Location = new Point(459, 347);
+            button4.Name = "button4";
+            button4.Size = new Size(256, 65);
+            button4.TabIndex = 2;
+            button4.Text = "CLEAR";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Log Program";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ ceng211_oop_hw4/Form1.cs
@@ -0,0 +1,78 @@
+using System.Xml;
+
+namespace ceng211_oop_hw4
+{
+    public partial class Form1 : Form
+    {
+        public string path = @"R:\C#\Homework4\logs.txt";
+        DateTime now = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                FileStream file = new FileStream(path, FileMode.Create);
+                file.Close();
+            }
+
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                writer.WriteLine($"{now} | user opened program.");
+                writer.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                writer.WriteLine($"{now} | user clicked useless button.");
+                writer.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                using (StreamWriter writer = File.AppendText(path))
+                {
+                    writer.WriteLine($"{now} | {textBox1.Text}");
+                    writer.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please write something.", "Write Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.Delete(path);
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(path))
+            {
+                StreamReader reader = new StreamReader(path);
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(line);
+                }
+                reader.Close();
+            }
+            else
+            {
+                MessageBox.Show("File doesn't exist now. Add something.", "Write Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
